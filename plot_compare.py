9991e1eb48fc7148faf77d5b11cbf96aef2a891a
minashe <minashe@math.uio.no>
2022-10-19 15:58:34 +0200

    fikset på compare

--- plot_compare.py
+++ plot_compare.py
@@ -32,9 +32,9 @@ ax[0, 1].loglog(
 ax[1, 0].set_title("LinkedList (get)")
 ax[1, 0].loglog(linked_list_get.T[0], linked_list_get.T[1], label="Time")
 ax[1, 0].loglog(
-    array_list_get.T[0],
-    0.005 * np.ones_like(linked_list_get.T[0]),
-    label="O(1)",
+    array_list_insert.T[0],
+    0.001 * array_list_insert.T[0],
+    label="O(n)",
     linestyle="--",
 )
 
@@ -42,11 +42,12 @@ ax[1, 0].loglog(
 ax[1, 1].set_title("LinkedList (insert)")
 ax[1, 1].loglog(linked_list_insert.T[0], linked_list_insert.T[1], label="Time")
 ax[1, 1].loglog(
-    linked_list_insert.T[0],
-    0.001 * linked_list_insert.T[0],
-    label="O(n)",
+    array_list_get.T[0],
+    0.005 * np.ones_like(array_list_get.T[0]),
+    label="O(1)",
     linestyle="--",
 )
+
 for axi in ax.flatten():
     axi.legend()
     axi.grid()
